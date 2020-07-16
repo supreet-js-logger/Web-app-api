b97c241186c37442359eb16675a84016b5dad85f
Supreet Sethi <supreet.s.sethi@gmail.com>
2020-07-16 22:12:32 +0530

    adding routes for logs in route files

---
+++ routes/logs.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.status(200).json({ success: true, msg: "list of all logs" });
+});
+
+module.exports = router;
