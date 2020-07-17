ae96f6dc21e47bc388b3b07114cda909274aea20
Supreet Sethi <supreet.s.sethi@gmail.com>
2020-07-17 09:53:19 +0530

    adding mongoose support to or application

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-//Route files
-const logs = require("./routes/logs");
+const connectDB = require("./config/db");
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+//connect to database
+connectDB();
+
+//Route files
+const logs = require("./routes/logs");
 
 const app = express();
 
@@ -16,7 +21,14 @@ app.use("/api/v1/logs", logs);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // close & exit process
+  server.close(() => process.exit(1));
+});
