5690c3778c0e3a421c4216c3f7c56086ad3be702
Supreet Sethi <supreet.s.sethi@gmail.com>
2020-07-19 01:52:48 +0530

    adding succsess status to logout url

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const bodyParser = require("body-parser");
 const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 const cors = require("cors");
+const Sentry = require("@sentry/node");
 const cookieParser = require("cookie-parser");
+Sentry.init({
+  dsn:
+    "https://b6013527dc804207bb3c1395c483acb6@o420201.ingest.sentry.io/5343580",
+});
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -21,6 +26,9 @@ const auth = require("./routes/auth");
 const projects = require("./routes/projects");
 
 const app = express();
+
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
 
 // Dev logging middleware
 process.env.NODE_ENV === "development" && app.use(morgan("dev"));
@@ -52,6 +60,22 @@ app.use("/api/v1/logs", logs);
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/projects", projects);
 
+app.get("/debug-sentry", function mainHandler(req, res) {
+  Sentry.captureMessage("Something went wrong");
+
+  myUndefinedFunction();
+});
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+// app.use(function onError(err, req, res, next) {
+//   // The error id is attached to `res.sentry` to be returned
+//   // and optionally displayed to the user for support.
+//   res.statusCode = 500;
+//   res.end(res.sentry + "\n");
+// });
+
 // setting up custom error handler
 app.use(errorHandler);
 
@@ -65,18 +89,18 @@ const server = app.listen(PORT, () =>
   ),
 );
 
-// Handle unhandled promise rejections
-process.on("unhandledRejection", (err, promise) => {
-  // eslint-disable-next-line no-console
-  console.log(`Unhandled Rejection at:${promise} reason:${err}`.red);
-  // close & exit process
-  server.close(() => process.exit(1));
-});
+// // Handle unhandled promise rejections
+// process.on("unhandledRejection", (err, promise) => {
+//   // eslint-disable-next-line no-console
+//   // console.log(`Unhandled Rejection at:${promise} reason:${err}`.red);
+//   // close & exit process
+//   server.close(() => process.exit(1));
+// });
 
-process.on("uncaughtException", (err, origin) => {
-  // eslint-disable-next-line no-console
-  console.log(
-    process.stderr.fd,
-    `Caught exception: ${err}\n${`Exception origin: ${origin}`.red}`,
-  );
-});
+// process.on("uncaughtException", (err, origin) => {
+//   // eslint-disable-next-line no-console
+//   // console.log(
+//   //   process.stderr.fd,
+//   //   `Caught exception: ${err}\n${`Exception origin: ${origin}`.red}`,
+//   // );
+// });
